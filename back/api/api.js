82191c40549ca17aec9a7cd4cfe95e04151141ab
Puccibat <baptiste.puccinelli@gmail.com>
2019-07-10 10:19:14 +0200

    Init connection BDD

--- back/api/api.js
+++ back/api/api.js
@@ -2,10 +2,19 @@ const express = require('express');
 const fs = require('fs');
 var app = express();
 
+var mysqlConnection = mysql.createConnection({
+    host: '',
+    user: '',
+    password: '',
+    database: ''
+});
 
-
-
-
+mysqlConnection.connect((err) => {
+    if (!err)
+        console.log('BDD connecté !')
+    else
+        console.log('Erreur de connection à la BDD : ' + JSON.stringify(err, undefined, 2));
+});
 
 
 app.listen(8082);
