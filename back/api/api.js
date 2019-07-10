fc74b9ae5ad50dc46a14bbafbe2f39101bdcb737
Puccibat <baptiste.puccinelli@gmail.com>
2019-07-10 10:29:43 +0200

    init GET users

--- back/api/api.js
+++ back/api/api.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const fs = require('fs');
+const bodyParser = require('body-parser');
 var app = express();
+
+app.use(bodyParser.json());
 
 var mysqlConnection = mysql.createConnection({
     host: '',
@@ -14,6 +17,16 @@ mysqlConnection.connect((err) => {
         console.log('BDD connecté !')
     else
         console.log('Erreur de connection à la BDD : ' + JSON.stringify(err, undefined, 2));
+});
+
+//get USERS
+app.get('/test', (req, res) => {
+    mysqlConnection.query('SELECT * FROM ???', (err, rows, fields) => {
+        if (!err)
+        console.log(rows);
+        else
+        console.log(err);
+    })
 });
 
 
